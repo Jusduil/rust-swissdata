20e145a8db3059d58e154c3fded9cc1bac87b0f9
Jus'duil <jusduil.hacker@gmail.com>
2023-01-02 10:23:22 +0100

    [FSO] Add meta information

--- src/fso/mod.rs
+++ src/fso/mod.rs
@@ -6,3 +6,95 @@ pub mod communes;
 //pub mod communes_historical;
 
 pub mod asset;
+
+use internal::*;
+mod internal {
+    use crate::tools::message::LinkedMessage;
+    pub fn editor() -> LinkedMessage<&'static str> {
+        [
+            (
+                "en",
+                "Federal Statistical Office",
+                "https://www.bfs.admin.ch/bfs/en/home.html",
+            ),
+            (
+                "de",
+                "Bundesamt für Statistik",
+                "https://www.bfs.admin.ch/bfs/de/home.html",
+            ),
+            (
+                "fr",
+                "Office fédéral de la statistique",
+                "https://www.bfs.admin.ch/bfs/fr/home.html",
+            ),
+            (
+                "it",
+                "Ufficio federale di statistica",
+                "https://www.bfs.admin.ch/bfs/it/home.html",
+            ),
+            (
+                "rm",
+                "Uffizi federal da statistica",
+                "https://www.bfs.admin.ch/bfs/rm/home.html",
+            ),
+        ]
+        .into_iter()
+        .collect()
+    }
+
+    pub fn copyright() -> LinkedMessage<&'static str> {
+        [
+            (
+                "en",
+                "Federal Statistical Office - Legal framework",
+                "https://www.bfs.admin.ch/bfs/en/home/fso/swiss-federal-statistical-office/legal-framework.html",
+            ),
+            (
+                "de",
+                "Bundesamt für Statistik - Rechtliche Hinweise",
+                "https://www.bfs.admin.ch/bfs/de/home/bfs/bundesamt-statistik/rechtliche-hinweise.html",
+            ),
+            (
+                "fr",
+                "Office fédéral de la statistique - Informations juridiques",
+                "https://www.bfs.admin.ch/bfs/fr/home/ofs/office-federal-statistique/informations-juridiques.html",
+            ),
+            (
+                "it",
+                "Ufficio federale di statistica - Basi legali",
+                "https://www.bfs.admin.ch/bfs/it/home/ust/ufficio-federale-statistica/basi-legali.html",
+            ),
+        ]
+        .into_iter()
+        .collect()
+    }
+    pub fn terms<S>(title: S) -> LinkedMessage<S>
+    where
+        S: From<&'static str> + Default + AsRef<str> + Clone,
+    {
+        [
+            (
+                "en".into(),
+                title.clone(),
+                "https://www.bfs.admin.ch/bfs/en/home/fso/swiss-federal-statistical-office/terms-of-use.html".into(),
+            ),
+            (
+                "de".into(),
+                title.clone(),
+                "https://www.bfs.admin.ch/bfs/de/home/bfs/bundesamt-statistik/nutzungsbedingungen.html".into(),
+            ),
+            (
+                "fr".into(),
+                title.clone(),
+                "https://www.bfs.admin.ch/bfs/fr/home/ofs/office-federal-statistique/conditions-utilisation.html".into(),
+            ),
+            (
+                "it".into(),
+                title.clone(),
+                "https://www.bfs.admin.ch/bfs/it/home/ust/ufficio-federale-statistica/condizioni-uso.html".into(),
+            ),
+        ]
+        .into_iter()
+        .collect()
+    }
+}
